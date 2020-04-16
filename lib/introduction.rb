e4b1b9371d5ce3be12d5fbad0ea705993ebb843d
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-16 00:41:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def introduction ("Josh")
+def #introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language (Dan, language)
+def #introduction_with_language (Dan, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
